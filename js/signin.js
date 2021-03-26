598f50be7eb13ff531703a81ff1bbcfde7ac393e
YOUR NAME <you@example.com>
2021-03-26 14:29:24 +0300

    omar new

---
+++ js/signin.js
@@ -0,0 +1,10 @@
+
+    function login() {
+     document.getElementById("box1").style.display = "block";
+        document.getElementById("box2").style.display = "none";
+    }
+
+    function signup() {
+        document.getElementById("box1").style.display = "none";
+        document.getElementById("box2").style.display = "block";
+    }
